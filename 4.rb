6ba5f759de24e2895d257673951a7d75642bea5e
Chris Lundquist <chris.lundquist@github.com>
2018-06-11 12:37:35 -0700

    comments

--- 4.rb
+++ 4.rb
@@ -17,9 +17,15 @@ class Integer
   end
 end
 
-# We know the factors we are looking for are probably in the 900s
+# We know the factors we are looking for are probably in the 900s,
+# so generate all the numbers that could be in that range
+# Grab only the candidates that are actually palindromes
 palindromes = (900**2..999**2).select { |i| i.digits == i.digits.reverse }
+
+# factorize our candidates and grab ones that have two 3 digit factors
 palindromes.select! { |i| i.paired_up_factors.any? { |a,b| a.digits.size == 3 and b.digits.size == 3 } }
+
+# Now get the biggest one that meets our constraints
 answer = palindromes.max
 puts answer
 puts answer.paired_up_factors.select { |a,b| a.digits.size == 3 and b.digits.size == 3 }.flatten.inspect
