f8df3ecfbce8354baaa8d96acf151418b922f4bb
Chris Lundquist <chris.lundquist@github.com>
2018-07-03 22:02:41 -0700

    solved 79

---
+++ 79.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+# start at the end and work right to left
+# we see there are no possible options to the right of 0
+# We see 8's and 9's to the left of 0, but we see "890"
+# We rinse and repeat this step until we always see 7 to the left
+
+puts "73162890"
